d6fb48056af711550e513c2cb375fbc1e2967dff
Edwin Alexander Rendon <36104986+eRendon@users.noreply.github.com>
2021-10-19 09:27:52 -0500

    sureti to deploy

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import app from "./plugins/app";
+/*
+ToDo TailWind CSS
+*/
+import './assets/css/index.css'
+
+/*
+ToDo Plugins
+*/
+
+import './plugins'
+import { authStorage } from './storage'
+
+app.mount('#app')
+
+await authStorage.actions.init()
